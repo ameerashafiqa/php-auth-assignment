d64601b5a90a1534715683d2c5c7e18cb7f7f79b
ameerashafiqa <ameeraashafiqaa@gmail.com>
2021-09-16 02:07:43 +0800

    logingit add

--- login.php
+++ login.php
@@ -2,18 +2,16 @@
    if(isset($_POST['submit'])){
         $d = file_get_contents("users.txt");
         $data = explode("\n", $d);
-        $Username = $_POST['Username'];
-        $Password = $_POST['Password'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
         foreach ($data as $row => $data) {
 
             $row_user = explode('|', $data);
             $users = @(strtolower($row_user[0]));
             $pass = @trim(strtolower($row_user[1]), "\r");
 
-            if (strcmp($users,$Username) === 0 && strcmp($pass,$Password) === 0) {
-                echo '<script>     alert("Success")
-                                window.location = (\"index.php\")
-                                </script>';
+            if (strcmp($users,$username) === 0 && strcmp($pass,$password) === 0) {
+                echo '<script>alert("Success")</script>';
                 break;
             }else{
                 echo '<script>alert("Failed")</script>';
@@ -23,6 +21,7 @@
 
     }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -46,8 +45,8 @@
 <body>
   <form class="box" action="" method="post">
     <h1>Login</h1>
-    <input type="text" autocomplete="off" name="Username" id="Username" placeholder="Username">
-    <input type="Password" name="Password" id="Password" placeholder="Password">
+    <input type="text" autocomplete="off" name="username" id="username" placeholder="Username">
+    <input type="password" name="password" id="password" placeholder="Password">
     <input type="submit" name="submit" value="Login">
   </form>
 </body>
