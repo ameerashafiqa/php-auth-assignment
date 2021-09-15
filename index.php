1e23b034e4fb77ab6d904f948f5d398a730d7b88
ameerashafiqa <ameeraashafiqaa@gmail.com>
2021-09-16 01:55:27 +0800

    index

--- index.php
+++ index.php
@@ -1,8 +1,43 @@
 <?php
+   if(isset($_POST['submit'])){
+        $d = file_get_contents("users.txt");
+        $data = explode("\n", $d);
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        foreach ($data as $row => $data) {
 
-/*
-* This file should be a secure protected file, which cannot be accessed unless the user
-* has successfully logged in with his/her credentials from users.txt.
-* 
-* This file should display a dashboard with data from the northwind database
-*/
+            $row_user = explode('|', $data);
+            $users = @(strtolower($row_user[0]));
+            $pass = @trim(strtolower($row_user[1]), "\r");
+
+            if (strcmp($users,$username) === 0 && strcmp($pass,$password) === 0) {
+                echo '<script>alert("Success")</script>';
+                break;
+            }else{
+                echo '<script>alert("Failed")</script>';
+                break;
+        }
+        }
+
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method ="POST">
+  <label for="username">First name:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">Last name:</label><br>
+  <input type="password" id="password" name="password"><br><br>
+  <input type="submit" name="submit" value="Login">
+</form> 
+
+
+
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,5 +1,54 @@
 <?php
-/*
-* This file should display the login form. Login and authentication process should use user credentials
-* from the file users.txt
-*/
+   if(isset($_POST['submit'])){
+        $d = file_get_contents("users.txt");
+        $data = explode("\n", $d);
+        $Username = $_POST['Username'];
+        $Password = $_POST['Password'];
+        foreach ($data as $row => $data) {
+
+            $row_user = explode('|', $data);
+            $users = @(strtolower($row_user[0]));
+            $pass = @trim(strtolower($row_user[1]), "\r");
+
+            if (strcmp($users,$Username) === 0 && strcmp($pass,$Password) === 0) {
+                echo '<script>     alert("Success")
+                                window.location = (\"index.php\")
+                                </script>';
+                break;
+            }else{
+                echo '<script>alert("Failed")</script>';
+                break;
+        }
+        }
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>e-KenderaanMDHT</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/styleee.css">
+</head>
+
+<style>
+  body {
+    background-image: url(images/d.jpeg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+</style>
+
+<body>
+  <form class="box" action="" method="post">
+    <h1>Login</h1>
+    <input type="text" autocomplete="off" name="Username" id="Username" placeholder="Username">
+    <input type="Password" name="Password" id="Password" placeholder="Password">
+    <input type="submit" name="submit" value="Login">
+  </form>
+</body>
+</html>
