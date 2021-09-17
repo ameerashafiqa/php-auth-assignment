d609265e4ce5b9141f9c33cfcd7aaba9f55fcc1f
ameerashafiqa <ameeraashafiqaa@gmail.com>
2021-09-17 16:50:52 +0800

    ibdex

--- index.php
+++ index.php
@@ -1,43 +1,292 @@
 <?php
-   if(isset($_POST['submit'])){
-        $d = file_get_contents("users.txt");
-        $data = explode("\n", $d);
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        foreach ($data as $row => $data) {
+require_once 'includes.php';
 
-            $row_user = explode('|', $data);
-            $users = @(strtolower($row_user[0]));
-            $pass = @trim(strtolower($row_user[1]), "\r");
+session_start();
 
-            if (strcmp($users,$username) === 0 && strcmp($pass,$password) === 0) {
-                echo '<script>alert("Success")</script>';
-                break;
-            }else{
-                echo '<script>alert("Failed")</script>';
-                break;
-        }
-        }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    }
+    $login = (new Login())->logout();
+    die;
+}
 ?>
 
+
 <!DOCTYPE html>
-<html>
+<section class="loginform cf">
+    
+</section>
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title> 
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="assets/materialize/css/materialize.min.css" media="screen,projection" />
+    <!-- Bootstrap Styles-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- Morris Chart Styles-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+    <!-- Google Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="assets/js/Lightweight-Chart/cssCharts.css"> 
+</head>
+
 <body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle waves-effect waves-dark" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand waves-effect waves-dark" href="index.php"><i class="large material-icons">track_changes</i> <strong>test</strong></a>
+				
+		<div id="sideNav" href=""><i class="material-icons dp48">toc</i></div>
+            </div>
 
-<h2>HTML Forms</h2>
+            <ul class="nav navbar-top-links navbar-right"> 
 
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method ="POST">
-  <label for="username">First name:</label><br>
-  <input type="text" id="username" name="username"><br>
-  <label for="password">Last name:</label><br>
-  <input type="password" id="password" name="password"><br><br>
-  <input type="submit" name="submit" value="Login">
-</form> 
+				  <li><a class="dropdown-button waves-effect waves-dark" href="#!" data-activates="dropdown1"><i class="fa fa-user fa-fw"></i> <b>Logout</b> <i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+        </nav>
+		<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+
+<li>
+<center>
+        <br>
+        <?php
+        if (!isset($_SESSION['is_auth'])) :
+            echo '<b>login Required! you will redirect to login page</b>';
+            header("refresh:3; url=login.php");
+            die;
+        else:
+            $r = '<b>' . 'GoodBye ' . $_SESSION['username'] . ' Please Come Agian </b>';
+            $r .= '
+        <form action='. $_SERVER['PHP_SELF'] .' method="post" accept-charset="utf-8">
+            <input type="submit" value="Logout">
+        </form>';
+            echo $r;
+        endif;
+        ?>
+        <br>
+    </center>
+</li>
+</ul>
 
 
+	   <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
 
+                    <li>
+                        <a class="active-menu waves-effect waves-dark" href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                   
+                 
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+      
+		<div id="page-wrapper">
+		  <div class="header"> 
+                        <h1 class="page-header">
+                            Dashboard
+                        </h1>
+						<ol class="breadcrumb">
+					  <li><a href="#">Home</a></li>
+					  <li><a href="#">Dashboard</a></li>
+					  <li class="active">Data</li>
+					</ol> 
+									
+		</div>
+        <!--/. TABLE -->
+            <div id="page-inner">
+
+			<div class="dashboard-cards"> 
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+						<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image red">
+						<i class="material-icons dp48">import_export</i>
+						</div>
+						<div class="card-stacked red">
+						<div class="card-content">
+                        <div class="count"><?php   $result= mysqli_query($conn,"SELECT SUM(UnitPrice) AS totalsale FROM order_details");
+                                $row = mysqli_fetch_assoc($result); 
+                                $sum = $row['totalsale'];
+                                echo $sum
+                        ?>
+						</div>
+                        </div>
+						<div class="card-action">
+						<strong>Total Sale</strong>
+						</div>
+						</div>
+						</div>
+	 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+					
+						<div class="card horizontal cardIcon waves-effect waves-dark">
+						<div class="card-image orange">
+						<i class="material-icons dp48">shopping_cart</i>
+						</div>
+						<div class="card-stacked orange">
+						<div class="card-content">
+						<div class="count"><?php  $result= mysqli_query($conn,"SELECT OrderID FROM orders");
+												$row=mysqli_num_rows($result);
+                                                echo $row
+												?>
+						
+						</div>
+                        </div>
+						<div class="card-action">
+						<strong>Total Order</strong>
+						</div>
+						</div>
+						</div> 
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+				
+                </div>
+			   </div>
+               <!--/.TABLE END -->
+				<!-- /. ROW  --> 
+			
+					<!--	<div class="col-xs-12 col-sm-12 col-md-5"> 
+						     <div class="row">
+									<div class="col-xs-12"> 
+									<div class="card">
+										<div class="card-image donutpad">
+										  <div id="morris-donut-chart"></div>
+										</div> 
+										<div class="card-action">
+										  <b>Donut Chart Example</b>
+										</div>
+									</div>	
+								</div>
+							 </div>
+						</div><!--/.row-->
+					</div>
+					
+		 
+			<!--	<div class="row">
+				<div class="col-md-5"> 
+						<div class="card">
+						<div class="card-image">
+						 <div id="morris-line-chart"></div>
+						</div> 
+						<div class="card-action">
+						  <b>Line Chart</b>
+						</div>
+						</div>
+		  
+					</div>		
+					
+						<div class="col-md-7"> 
+					<div class="card">
+					<div class="card-image">
+					  <div id="morris-bar-chart"></div>
+					</div> 
+					<div class="card-action">
+					  <b> Bar Chart Example</b>
+					</div>
+					</div>					
+					</div>
+					
+				</div> 
+			 
+				
+				
+                <div class="row">
+                    <div class="col-xs-12">
+						<div class="card">
+					<div class="card-image">
+					  <div id="morris-area-chart"></div>
+					</div> 
+					<div class="card-action">
+					  <b>Area Chart</b>
+					</div>
+					</div>	 
+                    </div> 
+
+                </div>
+				<div class="row">
+				<div class="col-md-12">
+				
+					</div>		
+				</div> 	
+                <!-- /. ROW  -->
+
+	   
+				
+				
+				
+               
+					
+                       
+
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+			   <div class="fixed-action-btn horizontal click-to-toggle">
+    <a class="btn-floating btn-large red">
+      <i class="material-icons">menu</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating red"><i class="material-icons">track_changes</i></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul>
+  </div>
+		
+				<footer><p>All right reserved</p>
+				
+        
+				</footer>
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+	
+	<!-- Bootstrap Js -->
+    <script src="assets/js/bootstrap.min.js"></script>
+	
+	<script src="assets/materialize/js/materialize.min.js"></script>
+	
+    <!-- Metis Menu Js -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- Morris Chart Js -->
+    <script src="assets/js/morris/raphael-2.1.0.min.js"></script>
+    <script src="assets/js/morris/morris.js"></script>
+	
+	
+	<script src="assets/js/easypiechart.js"></script>
+	<script src="assets/js/easypiechart-data.js"></script>
+	
+	 <script src="assets/js/Lightweight-Chart/jquery.chart.js"></script>
+	
+    <!-- Custom Js -->
+    <script src="assets/js/custom-scripts.js"></script> 
+ 
 
 </body>
+
 </html>
